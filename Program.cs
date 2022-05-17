27abc048ca0b7a39858654be2d770974db9ae0bd
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-17 07:20:54 +0000

    added for testing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program{
+    public static void Main(){
+        Console.WriteLine("Hello world!");
+        PrintNNumbers(4);
+    }
+
+    public static void PrintNNumbers(int num){
+        for(int i = 0; i <= num; i++){
+            System.Console.WriteLine(i);
+        }
+    }
+}
